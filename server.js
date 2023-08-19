67f941384e544b16775ad1636fc00686a0f0bcfb
jlemenager <jeremy@unitedbyzero.com>
2023-08-19 11:15:31 -0400

    testing different fixes

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ app.use(cors({
     methods: 'GET, POST, PUT, DELETE'
 }))
 app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
+// parse application/json
+app.use(bodyParser.json())
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
