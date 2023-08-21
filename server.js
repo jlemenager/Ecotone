f356bbdc0cb857741b3907401229684ad9a27733
jlemenager <jeremy@unitedbyzero.com>
2023-08-21 00:02:43 -0400

    changed server allow origin header

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // parse application/json
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
