1e52422ed9306123d3abba06358bfbd35c36e52d
jlemenager <jeremy@unitedbyzero.com>
2023-08-22 01:54:58 -0400

    added access control allow origin header

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // parse application/json
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
