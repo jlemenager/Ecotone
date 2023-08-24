67a60d9637cf06b95ac2a1f007512dd629187965
jlemenager <jeremy@unitedbyzero.com>
2023-08-24 00:05:52 -0400

    test

--- server.js
+++ server.js
@@ -13,15 +13,15 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 // parse application/json
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
 db.on('error', console.error.bind(console, 'MongoDB not connecting'))
 
-app.listen(PORT, ()=>console.log(`Server running on Port ${PORT}`))
+app.listen(PORT,'0.0.0.0', ()=>console.log(`Server running on Port ${PORT}`))
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const appRouter = require('./routes/appRouter')
 app.use('/api', appRouter)
