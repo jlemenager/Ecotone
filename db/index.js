53eb483d4372b487a1d1c02d2d4ca049ca6ea6bd
jlemenager <jeremy@unitedbyzero.com>
2023-08-23 23:47:05 -0400

    reseeded database

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-.connect('mongodb+srv://jlemenager:Jlhl1996!@cluster0.flwiryj.mongodb.net/ecotoneDatabase?retryWrites=true&w=majority')
+.connect('mongodb+srv://jlemenager:Jlhl1996@cluster0.flwiryj.mongodb.net/ecotoneDatabase?retryWrites=true&w=majority')
 .then(()=>console.log('MongoDB is running'))
 .catch((e)=>console.error('Connection error to MongoDB', e.message))
 
